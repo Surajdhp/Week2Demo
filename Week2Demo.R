36f08b5b0ce9d6b20779dd65e74cb427dac6d42c
Surajdhp <L00144473@student.lyit.ie>
2019-03-21 09:54:22 +0000

    ic

--- Week2Demo.R
+++ Week2Demo.R
@@ -2,3 +2,15 @@ x <- 2
 x
 y<- 5
 y
+
+
+c(10, 150, 30, 40, 55.6)
+#These all are integers except the last
+
+p <- c (1, 5, 6, 7)
+q <- c (3, 2, 3)
+r <- p+q
+r
+install.packages("swirl")
+library(swirl)
+swirl()
